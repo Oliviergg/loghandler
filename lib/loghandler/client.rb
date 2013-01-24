23be66b4862c4a607ce6f5e1fc6e34e149f1c2bd
Olivier Gosse-Gardet <olivier.gosse.gardet@gmail.com>
2013-01-24 13:18:05 +0100

    Added : uid + node_name

--- lib/loghandler/client.rb
+++ lib/loghandler/client.rb
@@ -1,4 +1,11 @@
+require 'SecureRandom'
 class Loghandler::Client < EM::Connection
+  def initialize(args)
+    super
+    @options = args
+    @reconnect_try = 0
+  end
+  
   def hostname
     Socket.gethostname
   end
@@ -13,17 +20,31 @@ class Loghandler::Client < EM::Connection
     ensure
       Socket.do_not_reverse_lookup = orig
   end
+
+  def connection_completed
+    @reconnect_try = 0
+  end
+  
+  def unbind
+    @reconnect_try+=1
+    raise "Connection Lost - Max Try Reconnect #{@options[:max_reconnect]}" if @reconnect_try > @options[:max_reconnect]
+    EM.add_timer(1) do reconnect  @options[:url], @options[:port] end
+  end
   
   def self.run(options)
     EM.run do
-      Signal.trap("INT") do
-        EM.stop
-      end
-      @@options = options
+      # EM.error_handler{ |e|
+      #   puts "Error raised during event loop: #{e.message} "
+      # }
+      Signal.trap("INT") { EM.stop }
+      options.merge!({max_reconnect:10})
+      options.merge!({uid:SecureRandom.hex(10)}) if options[:uid].nil?
       channel = EventMachine::Channel.new
-      client = EventMachine.connect options[:url], options[:port], Loghandler::Client
+      
+      client = EventMachine.connect options[:url], options[:port], Loghandler::Client, options
+      # TODO : move into Loghandler::Client
       channel.subscribe do |data|
-          to_send={content:data,logged_at:Time.now}.merge(@@options)
+          to_send={content:data,logged_at:Time.now}.merge(options)
           client.send_data "#{to_send.to_json}\n"
       end
       Loghandler::Tailer.new(options,channel)
